b6354cc0b70a75e848e3be149f49d63a346dcbc4
Patrik Batista <pcr_batista@homail.com>
2021-09-29 15:58:59 -0300

    inicia projeto, requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,85 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+
+import App from '../App';
+
+describe('01-  testa o componete App', () => {
+  it('01- testa se a aplicaçao é redirecionada para a pagina principal, HOME', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const homeLink = screen.getByRole('link', {
+      name: /home/i,
+    });
+    expect(homeLink).toBeInTheDocument();
+
+    userEvent.click(homeLink);
+
+    const homeText = screen.getByRole('heading', {
+      level: 1,
+      name: /pokédex/i,
+    });
+    expect(homeText).toBeInTheDocument();
+  });
+});
+
+test('02- testa se a aplicaçao é redirecionada para a pagina principal, ABOUT', () => {
+  const customHistory = createMemoryHistory();
+
+  render(
+    <Router history={ customHistory }>
+      <App />
+    </Router>,
+  );
+
+  customHistory.push('/about');
+
+  const aboutText = screen.getByRole('heading', {
+    level: 2,
+    name: /about pokédex/i,
+  });
+  expect(aboutText).toBeInTheDocument();
+});
+
+test('03- testa se a aplicaçao é redirecionada para a pagina, FAVORITE POKEMONS', () => {
+  const customHistory = createMemoryHistory();
+
+  render(
+    <Router history={ customHistory }>
+      <App />
+    </Router>,
+  );
+
+  customHistory.push('/favorites');
+
+  const favoritesText = screen.getByRole('heading', {
+    level: 2,
+    name: /favorite pokémons/i,
+  });
+  expect(favoritesText).toBeInTheDocument();
+});
+
+test('04- testa se a aplicaçao é redirecionada para a pagina, NOT FOUND', () => {
+  const customHistory = createMemoryHistory();
+
+  render(
+    <Router history={ customHistory }>
+      <App />
+    </Router>,
+  );
+
+  customHistory.push('/xablau');
+
+  const notFoundText = screen.getByRole('heading', {
+    level: 2,
+    name: /Page requested not found/i,
+  });
+  expect(notFoundText).toBeInTheDocument();
+});
