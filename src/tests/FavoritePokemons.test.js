88f495d3b5669f4a90328918bd448b04dc3f5998
Patrik Batista <pcr_batista@homail.com>
2021-10-04 13:46:45 -0300

    fianaliza requisitos 3 e 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,45 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
+
+import App from '../App';
+
+test('01- testa se a page tras informacoes sobre a pokedex', () => {
+  const customHistory = createMemoryHistory();
+
+  render(
+    <Router history={ customHistory }>
+      <App />
+    </Router>,
+  );
+  customHistory.push('/favorites');
+
+  const textFavorite = screen.getByText(/No favorite pokemon found/i);
+  expect(textFavorite).toBeInTheDocument();
+});
+// falta o segundo teste
+test('Testa se é exibido todos os cards de pokémons favoritados', () => {
+  const customHistory = createMemoryHistory();
+
+  render(
+    <Router history={ customHistory }>
+      <App />
+    </Router>,
+  );
+  customHistory.push('/');
+  const detailLink = screen.getByRole('link', {
+    name: /more details/i,
+  });
+  userEvent.click(detailLink);
+
+  const checkedButton = screen.getByRole('checkbox');
+  userEvent.click(checkedButton);
+  expect(checkedButton).toBeInTheDocument();
+
+  customHistory.push('/favorites');
+
+  const imageFavorites = screen.getByText('Pikachu');
+  expect(imageFavorites).toBeInTheDocument();
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+
+import App from '../App';
+
+test('01- testa se a page tras informacoes sobre a pokedex', () => {
+  const customHistory = createMemoryHistory();
+
+  render(
+    <Router history={ customHistory }>
+      <App />
+    </Router>,
+  );
+  customHistory.push('/xablau');
+
+  const notFoundText = screen.getByRole('heading', {
+    level: 2,
+    name: /Page requested not found/i,
+  });
+  expect(notFoundText).toBeInTheDocument();
+});
+
+test('Teste se página mostra a imagem', () => {
+  const customHistory = createMemoryHistory();
+  const alt = 'Pikachu crying because the page requested was not found';
+  render(
+    <Router history={ customHistory }>
+      <App />
+    </Router>,
+  );
+  customHistory.push('/xablau');
+
+  const imgNotFound = screen.getByAltText(alt);
+  expect(imgNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
