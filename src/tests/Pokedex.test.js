c02a122c03cfd4077a49a774618a2bf5f44962db
Patrik Batista <pcr_batista@homail.com>
2021-10-04 18:02:08 -0300

    inicia requisito 6 e finaliza o requisito 7

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,70 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
+
+import App from '../App';
+
+test('teste se é renderizado um card com as informações de determinado pokémon.', () => {
+  const customHistory = createMemoryHistory();
+
+  render(
+    <Router history={ customHistory }>
+      <App />
+    </Router>,
+  );
+  customHistory.push('/');
+
+  const pokemonName = screen.getByTestId('pokemon-name');
+  expect(pokemonName).toBeInTheDocument();
+
+  const pokemonType = screen.getByTestId('pokemon-type');
+  expect(pokemonType).toBeInTheDocument();
+
+  const pokemonWeigth = screen.getByTestId('pokemon-weight');
+  console.log(pokemonWeigth.value);
+  expect(pokemonWeigth).toHaveValue(/Average weight: 6.0 kg/i);
+
+  const pokemonImage = screen.getByRole('img');
+  expect(pokemonImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  expect(pokemonImage).toHaveAttribute('alt', 'Pikachu sprite');
+});
+
+test('Teste se ao clicar no link,redireciona para a página de detalhes', () => {
+  const customHistory = createMemoryHistory();
+
+  render(
+    <Router history={ customHistory }>
+      <App />
+    </Router>,
+  );
+  customHistory.push('/');
+
+  const linkDetails = screen.getByText('More details');
+  expect(linkDetails).toBeInTheDocument();
+  expect(linkDetails).toHaveAttribute('href', '/pokemons/25');
+  userEvent.click(linkDetails);
+  const summaryText = screen.getByText('Summary');
+  expect(summaryText).toBeInTheDocument();
+});
+
+test('Teste se existe um ícone de estrela nos Pokémons favoritados', () => {
+  const customHistory = createMemoryHistory();
+
+  render(
+    <Router history={ customHistory }>
+      <App />
+    </Router>,
+  );
+  customHistory.push('/');
+
+  const linkDetails = screen.getByText('More details');
+  userEvent.click(linkDetails);
+  const favorite = screen.getByText(/Pokémon favoritado/i);
+  userEvent.click(favorite);
+
+  const starIcon = screen.getByAltText('Pikachu is marked as favorite');
+  expect(starIcon).toBeInTheDocument();
+  expect(starIcon).toHaveAttribute('alt', 'Pikachu is marked as favorite');
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,87 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
+
+import App from '../App';
+
+test('Teste as informações detalhadas do Pokémon', () => {
+  const customHistory = createMemoryHistory();
+  render(
+    <Router history={ customHistory }>
+      <App />
+    </Router>,
+  );
+  customHistory.push('/');
+
+  const linkDetails = screen.getByText('More details');
+  expect(linkDetails).toBeInTheDocument();
+  expect(linkDetails).toHaveAttribute('href', '/pokemons/25');
+  userEvent.click(linkDetails);
+  const detailsPokemon = screen.getByRole('heading', {
+    level: 2,
+    name: 'Pikachu Details',
+  });
+  expect(detailsPokemon).toBeInTheDocument();
+
+  expect(linkDetails).not.toBeInTheDocument();
+  const summaryText = screen.getByRole('heading', {
+    level: 2,
+    name: /summary/i,
+  });
+  expect(summaryText).toBeInTheDocument();
+  const paragraphText = screen.getByText(/This intelligent/i);
+  expect(paragraphText).toBeInTheDocument();
+});
+
+test('teste seção de mapas do pokemon', () => {
+  const customHistory = createMemoryHistory();
+  render(
+    <Router history={ customHistory }>
+      <App />
+    </Router>,
+  );
+  customHistory.push('/');
+
+  const linkDetails = screen.getByText('More details');
+  userEvent.click(linkDetails);
+
+  const locationText = screen.getByRole('heading', {
+    level: 2,
+    name: /Game Locations of Pikachu/i,
+  });
+  expect(locationText).toBeInTheDocument();
+
+  const nameLocationOne = screen.getByText('Kanto Viridian Forest');
+  expect(nameLocationOne).toBeInTheDocument();
+
+  const mapOne = screen.getAllByAltText('Pikachu location');
+  console.log(mapOne);
+
+  mapOne.forEach((map) => {
+    expect(map).toBeInTheDocument();
+    expect(map).toHaveAttribute('alt', 'Pikachu location');
+    expect(map).toHaveAttribute('src', map.src);
+  });
+  // expect(mapOne).toBeInTheDocument();
+  // expect(mapOne).toHaveAttribute('alt', 'Pikachu location');
+  // expect(mapOne).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+
+  const nameLocationTwo = screen.getByText('Kanto Power Plant');
+  expect(nameLocationTwo).toBeInTheDocument();
+
+  // const mapTwo = screen.getByRole('img', {
+  //   src: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+  // });
+  // expect(mapTwo).toBeInTheDocument();
+  // expect(mapTwo).toHaveAttribute('alt', 'Pikachu location');
+  // expect(mapTwo).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+
+  const labelText = screen.getByLabelText('Pokémon favoritado?');
+  expect(labelText).toBeInTheDocument();
+  userEvent.click(labelText);
+  expect(labelText).toBeChecked();
+  userEvent.click(labelText);
+  expect(labelText).not.toBeChecked();
+});
